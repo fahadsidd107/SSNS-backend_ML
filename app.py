76b0187d8b6a96dea5ea378e1c03fb73fb98977e
fahadSiddiqui-mset <muhammadfahad.siddiqui@mset.io>
2024-07-06 22:58:19 +0200

    vercel try

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def get_latest_data():
         print(f"Error: {e}")  # Log the error for debugging
         return jsonify({'error': str(e)}), 500
 
-# if __name__ == '__main__':
-#     app.run(debug=False, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
